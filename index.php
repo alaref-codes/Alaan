78c24247e6f5e170d96c955b6b5d76d0fafa70d8
Zakaria Khalaf <105569263+ZakariaKhalaf@users.noreply.github.com>
2024-06-11 17:02:44 +0200

    u

--- index.php
+++ index.php
@@ -855,7 +855,7 @@
 
             <div class="field-set mb20">
                 <span class="d-label">Message*</span>
-                <textarea name="Message" id="message" class="form-control" placeholder="Why are you interested in this service? Tell us more about the scope of your resources and the type of your business..." required></textarea>
+                <textarea name="Message" id="message" class="form-control" placeholder="لماذا أنت مهتم بهذه الخدمة؟ أخبرنا المزيد عن حدود مواردك ونوع مشروعك..." required></textarea>
             </div>
         </div>
     </div>
