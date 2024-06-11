bd180e0812e4a3015f516e77d1e74c6ff5ebbed0
Zakaria Khalaf <105569263+ZakariaKhalaf@users.noreply.github.com>
2024-06-11 17:12:00 +0200

    update

--- index.php
+++ index.php
@@ -846,7 +846,7 @@
                     <option value="Alaan Platform as a Service">Alaan Platform as a Service</option>
                     <option value="Alaan S3 Storage as a Service">Alaan S3 Storage as a Service</option>
                     <option value="Alaan Migration Service">Alaan Migration Service</option>
-                    <option value="Alaan Multi-tenant backup and restore as a service">Multi-tenant backup and restore as a service</option>
+                    <option value="Alaan Multi-tenant backup and restore as a service">Alaan Multi-tenant backup and restore as a service</option>
                     <option value="Cloud and Datacenter Consultancy">Cloud and Datacenter Consultancy</option>
                     <option value="Rack rental and Colocation">Rack rental and Colocation</option>
                 </select>
