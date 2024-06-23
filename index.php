dacb3b9677644ca37218b6d5c9f422d8bbd756e5
Zakaria Khalaf <105569263+ZakariaKhalaf@users.noreply.github.com>
2024-06-23 10:20:56 +0200

    update bug

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
 <div class="row py-2">
     <div class="col-2 col-sm-1 col-md-3 col-lg-2 py-1 pe-md-0 mb-md-1">
 
-      <div class="d-inline-block d-md-block text-white text-center breaking-caret py-1 px-2"  style="background-color: #1974F9;">
+      <div class="d-inline-block d-md-block text-white text-center breaking-caret py-1 px-2" style="background-color: #1974F9;">
         
         <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" fill="currentColor" class="de-circle-2 bi bi-lightning-fill" viewBox="0 0 16 16">
           <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z"/>
